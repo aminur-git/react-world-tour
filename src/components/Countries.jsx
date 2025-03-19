529ad105d88f4fa7990ff5fd55cb06bb54b44a66
aminur-git <aminur.4mi@gmail.com>
2025-03-20 04:17:02 +0600

    mathar-pash-dye-gelo

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,18 +4,33 @@ import "../components/Country/countries.css"
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
 
+    const handleVisitedCountry = country => {
+      console.log(country)
+      const newVisitedCountries = [...visitedCountries, country]
+      setVisitedCountries(newVisitedCountries)
+    }
+
+
   return (
     <div className="headers " >
       <h3>Countries : {countries.length} </h3>
+      <div>
+        {
+          visitedCountries.map(country => <li>{country}</li>)
+        }
+      
+      </div>
       <div className="country-conrainer">
       {countries.map((country) => (
-        <Country country={country} key={country.cca3}></Country>
+        <Country country={country} key={country.cca3} handleVisitedCountry={handleVisitedCountry}></Country>
       ))}
       </div>
     </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,31 @@
 import { useState } from "react";
 import "../Country/country.css";
-const Country = ({ country }) => {
+const Country = ({country, handleVisitedCountry}) => {
   const { name, flags, capital, continents, population } = country;
 
-  const [visited, setVisited] = useState();
+  const [visited, setVisited] = useState(false);
 
   const handleVisited = () => {
     setVisited(!visited);
   };
 
+  console.log(handleVisitedCountry)
+
   return (
-    <div className={`country ${visited ? 'visited' : ''}`}>
-      <h3 style={{backgroundColor: visited ? 'skyBlue' : '', color: visited ? 'black' : '' }} className="highlight"> {name?.common}</h3>
+
+    
+    <div className={`country ${visited ? "visited" : ""}`}>
+        <h3></h3>
+      <h3
+        style={{
+          backgroundColor: visited ? "skyBlue" : "",
+          color: visited ? "black" : "",
+        }}
+        className="highlight"
+      >
+        {" "}
+        {name?.common}
+      </h3>
       <div className="">
         <div className="">
           <img className="image" src={flags.png} alt="" />
@@ -28,6 +42,7 @@ const Country = ({ country }) => {
           >
             {visited ? "Visited" : "Visit"}
           </button>
+          <button style={{marginLeft: '5px'}} onClick={() => {handleVisitedCountry(country.name.common)}} >Mark Visited</button>
         </div>
         <div className="visit">
           {visited
